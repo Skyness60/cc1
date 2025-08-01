f84cf4c174fc8d837d433aa45dba57815feebec7
Sami Perron <sami.perron60@gmail.com>
2025-08-01 17:24:07 +0200

    Add colored output for error messages in main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use colored::Colorize;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Arguments: {:?}", args);
+    if args.len() < 2
+    {
+        eprintln!("{}: {} no input files", "cc1".bold(), "fatal error:".red().bold());
+        eprintln!("compilation terminated.");
+        std::process::exit(1);
+    }
 }
